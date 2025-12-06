98bc05557dede99f0be4fae0013e0c16c1a66c4b
franzheinau <rifaldi0823666@gamail.com>
2025-12-06 20:01:07 +0800

    comit guest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>{{ config('Pthree', 'Pthree') }}</title>
+    <title>{{ config('app.name', 'Pthree') }}</title>
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.bunny.net">
