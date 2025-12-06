ec2d84602c37e2d4a6c77c9f3e89cb36b35773fc
franzheinau <rifaldi0823666@gamail.com>
2025-12-06 18:20:04 +0800

    Use Tailwind CDN on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    <!-- TAILWIND CDN (tanpa Vite, aman di Vercel) -->
+    <!-- TAILWIND CDN (TIDAK ADA VITE DI SINI) -->
     <script src="https://cdn.tailwindcss.com"></script>
 
     <style>
