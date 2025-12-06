3269ce24e2ac636eafc75ece5049c83ac2e0516c
franzheinau <rifaldi0823666@gamail.com>
2025-12-06 17:23:29 +0800

    Menambahka

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
